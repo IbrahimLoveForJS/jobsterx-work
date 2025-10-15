db15674bbc008c71879bc8e352cc7a394484e327
IbrahimLoveForJS <sarfarazalishah99@gmail.com>
2025-10-16 01:01:19 +0500

    ..

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,31 @@
-import React, { useState } from "react";
-import Header from "./components/Header";
-import ResumeProgress from "./components/ResumeProgress";
-import ResumeFormat from "./components/ResumeFormat";
-import ResumeDownloads from "./components/ResumeDownloads";
-import ATSTips from "./components/ATSTips";
+import React, { useState } from "react"
+import Header from "./components/Header"
+import ResumeProgress from "./components/ResumeProgress"
+import ResumeFormat from "./components/ResumeFormat"
+import ResumeDownloads from "./components/ResumeDownloads"
+import ATSTips from "./components/ATSTips"
 
 export default function App() {
-  const [country, setCountry] = useState("United States");
+  const [country, setCountry] = useState("United States")
 
   return (
-    <div className="ml-56 mr-20 min-h-screen bg-[#fbfbfd] py-8 px-4 sm:px-6 lg:px-8 flex justify-center">
-      <div className="w-full container-center mx-auto">
+    <div className="min-h-screen bg-[#fbfbfd] py-6 px-4 sm:px-6 lg:px-8 flex justify-center">
+      <div className="w-full max-w-5xl mx-auto space-y-6">
+        {/* Header */}
         <Header />
 
+        {/* Resume Progress */}
         <ResumeProgress />
 
+        {/* Resume Format */}
         <ResumeFormat country={country} setCountry={setCountry} />
 
+        {/* Resume Downloads */}
         <ResumeDownloads />
 
+        {/* ATS Tips */}
         <ATSTips />
       </div>
     </div>
-  );
+  )
 }
